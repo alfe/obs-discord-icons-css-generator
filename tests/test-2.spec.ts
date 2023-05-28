0cf5d4eb72ccaf7203bfa5a39ea8d5fba261ee2d
alfe <alfe10251@gmail.com>
2023-05-28 11:58:00 +0900

    add test

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -1,7 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-  await page.getByRole('button', { name: 'vertical' }).click();
-  await page.getByRole('button', { name: 'horizontal' }).click();
+  await page.goto('/');
+  await page.getByRole('button', { name: '日本語' }).click();
+  await page.getByRole('button', { name: '縦並び' }).click();
+  await page.getByRole('button', { name: '横並び' }).click();
+  await expect(page.locator('.MuiPaper-root:last-child')).toHaveText(/[class*="Voice_voiceStates__"]/);
+  await expect(page.locator('.MuiPaper-root:last-child')).toHaveText(/display: flex;/);
+  await expect(page.locator('.MuiPaper-root:last-child')).toHaveText(/flex-wrap: wrap;/);
 });
